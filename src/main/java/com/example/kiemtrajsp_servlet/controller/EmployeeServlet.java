6256c398099239edb6a2c890e35d54fed841d62b
Trangne2901 <tranthuyentrang29012005@gmail.com>
2024-12-10 15:18:00 +0700

    AddEmployee

--- src/main/java/com/example/kiemtrajsp_servlet/controller/EmployeeServlet.java
+++ src/main/java/com/example/kiemtrajsp_servlet/controller/EmployeeServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name="EmployeeServlet", value = "/employee")
+@WebServlet(name = "EmployeeServlet", value = "/employee")
 public class EmployeeServlet extends HttpServlet {
     private EmployeeService employeeService = new EmployeeServiceImpl();
 
@@ -24,6 +24,24 @@ public class EmployeeServlet extends HttpServlet {
 
         }
         switch (action) {
+            case "add":
+                showNewForm(request, response);
+                break;
+            default:
+                showEmployee(request, response);
+                break;
+
+        }
+    } @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "addEmployee":
+                addEmployee(request, response);
+                break;
             default:
                 showEmployee(request, response);
                 break;
@@ -37,5 +55,21 @@ public class EmployeeServlet extends HttpServlet {
         request.getRequestDispatcher("employee-list.jsp").forward(request, response);
     }
 
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("employee-form.jsp").forward(request, response);
+    }
+
+    private void addEmployee(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        int age = Integer.parseInt(request.getParameter("age"));
+        String position = request.getParameter("position");
+        String department = request.getParameter("department");
+        double salary = Double.parseDouble(request.getParameter("salary"));
+        int id = (int) (Math.random() * 10000);
+
+        Employee newEmployee = new Employee(id, name, age, position, department, salary);
+        employeeService.addEmployee(newEmployee);
+        response.sendRedirect("employee");
+    }
 
 }
